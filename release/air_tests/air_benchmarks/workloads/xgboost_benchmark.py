5da3f61b4a673fb6580f696eca92eeb35c6c2848
Neha Arora <neha.arora@microsoft.com>
2025-03-05 15:41:06 -0800

    provide account name

--- release/air_tests/air_benchmarks/workloads/xgboost_benchmark.py
+++ release/air_tests/air_benchmarks/workloads/xgboost_benchmark.py
@@ -74,7 +74,7 @@ def run_and_time_it(f):
 
 @run_and_time_it
 def run_xgboost_training(data_path: str, num_workers: int):
-    ds = data.read_parquet(data_path)
+    ds = ray.data.read_parquet(data_path, filesystem=adlfs.AzureBlobFileSystem(account_name="kuberaysa"))
     params = {
         "objective": "binary:logistic",
         "eval_metric": ["logloss", "error"],
